2e4fd7b6bf2d2d5473899f69fce5351e8d32d72d
An10113 <antranthanh1011@gmail.com>
2023-07-29 21:40:00 +0700

    author

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const Author = () => {
+  const {id} = useParams()
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    async function getData(){
+      const response = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`)
+      setData(response.data)
+      setLoading(true)
+    }
+    getData()
+  },[])
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -25,15 +37,15 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <img src={data.authorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
+                          {data.authorName}
+                          <span className="profile_username">{data.tag}</span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            {data.address}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -44,7 +56,7 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
+                      <div className="profile_follower">{`${data.followers} followers`}</div>
                       <Link to="#" className="btn-main">
                         Follow
                       </Link>
@@ -55,7 +67,7 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems author={data}/>
                 </div>
               </div>
             </div>
